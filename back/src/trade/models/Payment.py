8f6f624ec562f86dbc27127cb1ed66f07781d38e
Eduardo Villota <eduardouio7@gmail.com>
2024-09-05 05:36:57 -0500

    pagos

--- back/src/trade/models/Payment.py
+++ back/src/trade/models/Payment.py
@@ -1,13 +1,62 @@
 from common import BaseModel
 from django.db import models
+from trade.models import Invoice
+
+
+METHOD_CHOICES = [
+    ('transfer', 'Transferencia'),
+    ('check', 'Cheque'),
+    ('cash', 'Efectivo'),
+    ('other', 'Otro'),
+    ('credit_card', 'Tarjeta de Crédito')
+    ('credit_note', 'Nota de Crédito')
+]
 
 
 class Payment(BaseModel):
-    invoice = models.ForeignKey('Invoice', on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    payment_date = models.DateField()
-    payment_method = models.CharField(max_length=50)
-    notes = models.TextField(blank=True, null=True)
+    id = models.AutoField(
+        primary_key=True
+    )
+    invoice = models.ManyToManyField(
+        Invoice,
+        on_delete=models.CASCADE
+    )
+    date = models.DateField(
+        'Fecha de pago'
+    )
+    amount = models.DecimalField(
+        'Monto',
+        max_digits=10,
+        decimal_places=4
+    )
+    method = models.CharField(
+        'Metodo de pago',
+        max_length=50
+    )
+    bank = models.CharField(
+        'Banco',
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    nro_account = models.CharField(
+        'Nro de Cuenta',
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    nro_operation = models.CharField(
+        'Nro de Operación',
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    evidence = models.FileField(
+        'Comprobante',
+        upload_to='payments',
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return f"Pago {self.id} - Factura {self.invoice.id}"
