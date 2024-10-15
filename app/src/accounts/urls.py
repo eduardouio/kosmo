0bd5ea678231fc94fb801f3f06ecea50699bbc5d
Eduardo Villota <eduardouio7@gmail.com>
2024-10-15 05:45:43 -0500

    refactor

--- app/src/accounts/urls.py
+++ app/src/accounts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from .views import LoginTV, LogoutRV, HomeTV
 
-app_name = 'accounts'
-
-
 urlpatterns = [
     path('', HomeTV.as_view(), name='home'),
     path('accounts/login/', LoginTV.as_view(), name='login'),
