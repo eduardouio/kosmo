e08e72fb831f087afa71ac1bea4a5299b82c8b3c
Eduardo Villota <eduardouio7@gmail.com>
2024-10-15 04:03:09 -0500

    refactor

--- app/src/trade/urls.py
+++ app/src/trade/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import StockDiary
 
-app_name = 'trade'
 
 urlpatterns = [
     path('trade/stock/add/', StockDiary.as_view(), name='add-stock'),
--- app/src/trade/views/StockDiary.py
+++ app/src/trade/views/StockDiary.py
@@ -1,6 +1,40 @@
+import json
+import re
+from django.core import serializers
+from django.http import JsonResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from partners.models import Partner
+from products.models import Product
 
 
 class StockDiary(LoginRequiredMixin, TemplateView):
-    template_name = 'forms/stock.html'
+    template_name = 'forms/stock-form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = Product.objects.all()
+        partners = Partner.get_suppliers()
+        context['title_page'] = 'Disponibilidad'
+        context['products_json'] = serializers.serialize('json', products)
+        context['partners_json'] = serializers.serialize('json', partners)
+        context['products'] = products
+        context['partners'] = partners
+        return context
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        pattern = r"(\d+)(\w{2}) (\D+) (\d+\/?\d*) x (\d+) ([\d.\/]+)"
+        matches = re.findall(pattern, data['stock_text'].replace(',', '.'))
+        disponiblility = []
+        for item in matches:
+            disponiblility.append({
+                'quantity_box': item[0],
+                'box_model': item[1].upper(),
+                'product': item[2].upper(),
+                'length': item[3].split('/'),
+                'qty_stem_flower': item[4],
+                'stem_cost_price': item[5].split('/'),
+            })
+
+        return JsonResponse(disponiblility, safe=False)
