1ac010b3d644002f9f4cce17c20e739cf5428289
Eduardo Villota <eduardouio7@gmail.com>
2024-10-20 03:50:20 -0500

    modificamnos login

--- app/src/partners/urls.py
+++ app/src/partners/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PartnerDetailView
-
-app_name = 'partners'
+from .views import PartnerDetailView, PartnerListView, PartnerCreateView, PartnerUpdateView, PartnerDeleteView
 
 
 urlpatterns = [
-    path('partners/<int:pk>/', PartnerDetailView.as_view(), name='partner-detail'),
+    path('socios/', PartnerListView.as_view(), name='partner_list'),
+    path('socios/<int:pk>/', PartnerDetailView.as_view(), name='partner_detail'),
+    path('socios/nuevo/', PartnerCreateView.as_view(), name='partner_create'),
+    path('socios/actualizar/<int:pk>/', PartnerUpdateView.as_view(), name='partner_update'),
+    path('socios/eliminar/<int:pk>/', PartnerDeleteView.as_view(), name='partner_delete'),
 ]
