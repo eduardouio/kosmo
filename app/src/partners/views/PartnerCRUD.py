2c2100cc777b81df74e5e4cb52db1f8ffbf835b2
Eduardo Villota <eduardouio7@gmail.com>
2024-10-20 03:49:35 -0500

    proveedor

--- app/src/partners/views/PartnerCRUD.py
+++ app/src/partners/views/PartnerCRUD.py
@@ -12,11 +12,10 @@ from partners.models import Partner
 from partners.forms import PartnerForm
 
 
-# /partner/add
 class PartnerCreateView(LoginRequiredMixin, CreateView):
     model = Partner
     form_class = PartnerForm
-    template_name = 'forms/partner.html'
+    template_name = 'forms/partner-form.html'
 
     def get_context_data(self, *args, **kwargs):
         ctx = super(PartnerCreateView, self).get_context_data(*args, **kwargs)
@@ -29,90 +28,83 @@ class PartnerCreateView(LoginRequiredMixin, CreateView):
         return url
 
 
-# /partners/<pk>/edit/
 class PartnerUpdateView(LoginRequiredMixin, UpdateView):
     model = Partner
     form_class = PartnerForm
-    template_name = 'crm/partner-form.html'
+    template_name = 'forms/partner-form.html'
 
     def get_context_data(self, **kwargs):
-        ctx = super(PartnerUpdateView, self).get_context_data(**kwargs)
-        ctx['title_bar'] = 'Update Partner'
-        return ctx
+        context = super().get_context_data(**kwargs)
+        context['title_section'] = 'Actualizar Socio de Negocio {}'.format(
+            self.object.business_tax_id
+        )
+        context['title_page'] = 'Actualizar Socio de Negocio {}'.format(
+            self.object.business_tax_id
+        )
+
+        return context
 
     def get_success_url(self):
-        url = reverse_lazy('partner-detail', kwargs={'pk': self.object.id})
-        url += '?action=updated'
+        url = reverse_lazy('partner_detail', kwargs={'pk': self.object.pk})
+        url = f'{url}?action=updated'
         return url
 
 
-# /partners/<pk>/delete/
 class PartnerDeleteView(LoginRequiredMixin, DeleteView):
-    model = Partner
-    success_url = reverse_lazy('partner-list')
+    def get_redirect_url(self, *args, **kwargs):
+        partner = Partner.objects.get(pk=kwargs['pk'])
+        try:
+            partner.delete()
+            url = reverse_lazy('partner_list')
+            return f'{url}?action=deleted'
+        except Exception as e:
+            return f'{reverse_lazy("partner_detail",
+                                   kwargs={"pk": kwargs["pk"]}
+                                   )}?action = no_delete'
 
-    def get(self, request, *args, **kwargs):
-        return self.post(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        partner = self.get_object()
-        partner.delete()
-
-        return HttpResponseRedirect(self.success_url + '?action=deleted')
-
-
-# /partners/
 class PartnerListView(LoginRequiredMixin, ListView):
     model = Partner
-    template_name = 'crm/partner-list.html'
+    template_name = 'lists/partner_list.html'
     context_object_name = 'partners'
-
-    def get_queryset(self):
-        type_partner = self.request.GET.get('type')
-
-        if type_partner == 'customer':
-            return Partner.objects.filter(company=my_company, type='customer')
-        elif type_partner == 'supplier':
-            return Partner.objects.filter(company=my_company, type='supplier')
-        else:
-            return Partner.objects.filter(company=my_company)
+    ordering = ['name']
 
     def get_context_data(self, **kwargs):
-        ctx = super(PartnerListView, self).get_context_data(**kwargs)
-        ctx['action_type'] = None
-        ctx['filter'] = 'partners'
-        ctx['module_name'] = 'partners'
-        ctx['url_new'] = reverse_lazy('partner-create')
-        ctx['title_bar'] = 'Partners List'
-        ctx['type_a_class'] = 'badge border text-xs bg-cyan-600 uppercase text-white'
-        ctx['type_b_class'] = 'badge border text-xs bg-green-600 uppercase text-white'
-        ctx['url_filter_1'] = reverse_lazy('partner-list') + '?type=customer'
-        ctx['url_filter_2'] = reverse_lazy('partner-list') + '?type=supplier'
+        context = super(PartnerListView, self).get_context_data(**kwargs)
+        context['title_section'] = 'Socios de Negocio'
+        context['title_page'] = 'Listado de Socios de Negocio'
+        context['action'] = None
 
         if self.request.GET.get('action') == 'deleted':
-            ctx['action_type'] = 'success'
-            ctx['message'] = 'Partner deleted successfully'
-        return ctx
+            context['action_type'] = 'success'
+            context['message'] = 'Socio Eliminado Exitosamente'
+        return context
 
 
-# /crm/partners/<pk>/
 class PartnerDetailView(LoginRequiredMixin, DetailView):
     model = Partner
-    template_name = 'presentations/partner.html'
+    template_name = 'presentations/partner_presentation.html'
     context_object_name = 'partner'
 
     def get_context_data(self, **kwargs):
-        ctx = super(PartnerDetailView, self).get_context_data(**kwargs)
-        ctx['title_bar'] = 'Partner Detail'
-        ctx['action_type'] = None
-        if self.request.GET.get('action') == 'updated':
-            ctx['action_type'] = 'success'
-            ctx['message'] = 'Partner updated successfully'
-        if self.request.GET.get('action') == 'created':
-            ctx['action_type'] = 'success'
-            ctx['message'] = 'Partner created successfully'
-        if self.request.GET.get('action') == 'alert':
-            ctx['action_type'] = 'alert'
-            ctx['message'] = 'The Partner will be removed, cannot be undone.'
+        context = super(PartnerDetailView, self).get_context_data(**kwargs)
+        context['title_section'] = self.object.name
+        context['title_page'] = self.object.name
 
-        return ctx
+        if 'action' not in self.request.GET:
+            return context
+
+        context['action_type'] = self.request.GET.get('action')
+        message = ''
+
+        if context['action'] == 'created':
+            message = 'El socio de negocio ha sido creado con éxito.'
+        elif context['action'] == 'updated':
+            message = 'El socio de negocio ha sido actualizado con éxito.'
+        elif context['action'] == 'no_delete':
+            message = 'No es posible eliminar el socio de negocio. Existen dependencias.'
+        elif context['action'] == 'delete':
+            message = 'Esta acción es irreversible. ¿Desea continuar?.'
+
+        context['message'] = message
+        return context
