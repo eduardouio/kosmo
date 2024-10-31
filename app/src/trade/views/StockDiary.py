7e6998d3b05c68e4790ad7a82af8bc68e84d4824
Eduardo Villota <eduardouio7@gmail.com>
2024-10-31 12:21:18 -0500

    adptamos el modelo

--- app/src/trade/views/StockDiary.py
+++ app/src/trade/views/StockDiary.py
@@ -1,12 +1,11 @@
 import json
-import re
-from django.core import serializers
+from django.core.serializers import serialize
 from django.http import JsonResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from partners.models import Partner
 from products.models import Product
-from  common import StockAnalyzer
+from common import StockAnalyzer
 
 
 class StockDiary(LoginRequiredMixin, TemplateView):
@@ -17,8 +16,8 @@ class StockDiary(LoginRequiredMixin, TemplateView):
         products = Product.objects.all()
         partners = Partner.get_suppliers()
         context['title_page'] = 'Disponibilidad'
-        context['products_json'] = serializers.serialize('json', products)
-        context['partners_json'] = serializers.serialize('json', partners)
+        context['products_json'] = serialize('json', products)
+        context['partners_json'] = serialize('json', partners)
         context['products'] = products
         context['partners'] = partners
         return context
@@ -26,7 +25,22 @@ class StockDiary(LoginRequiredMixin, TemplateView):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
         stock_analyzer = StockAnalyzer()
+        print('llamado')
         disponiblility = stock_analyzer.get_stock(
             data['stock_text'], data['id_partner']
         )
-        return JsonResponse({}, status=200)
+        print('disponiblility')
+        disponiblility = self.serialize_dipo(disponiblility)
+        print('response')
+        return JsonResponse({"data": json.dumps(disponiblility)}, status=200)
+
+    def serialize_dipo(self, disponibility):
+        for itm in disponibility:
+            for p in itm['box_items']:
+                product = json.loads(serialize('json', [p['product']]))[0]
+                p['product'] = {
+                    'id': product['pk'], **product['fields']
+                }
+
+
+        return disponibility
