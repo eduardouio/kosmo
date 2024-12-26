3550fdaa86fc5db6a87f6093a9283c25c49855ac
Eduardo Villota <eduardouio7@gmail.com>
2024-12-26 13:59:29 -0500

    Colocamos un loader

---
+++ app/orders/src/components/Loader.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+<template>
+    <div class="d-flex flex-column justify-content-center align-items-center pt-5">
+            <div class="spinner-border text-primary p-5 m-5" role="status">
+            </div>
+            <span class="text-primary fs-5 blink">
+                Cargando...
+            </span>
+    </div>
+</template>
