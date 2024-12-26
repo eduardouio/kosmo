0999b7df1bddf4c328b7dd95ea18caf00cffa5d2
Eduardo Villota <eduardouio7@gmail.com>
2024-12-26 12:10:14 -0500

    Agregamos la url

--- app/orders/src/AppConfig.js
+++ app/orders/src/AppConfig.js
@@ -7,6 +7,8 @@ export const appConfig = {
     "csrfToken": csrfToken,
     "idStock": idStockDay,
     "urlLogo": apiBaseUrl + "/static/img/logo-kosmo.png",
+    "urlDispo": apiBaseUrl + "/api/stock_detail/" + idStockDay + "/",
+    "urlAllSuppliers": apiBaseUrl + "/api/partners/all-supliers/",
     "headers": {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
