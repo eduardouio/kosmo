013ca3ff4d53468212fdda8f692a63f56ac6bd5e
Eduardo Villota <eduardouio7@gmail.com>
2025-02-21 05:36:25 -0500

    refactor

--- app/src/partners/views/__init__.py
+++ app/src/partners/views/__init__.py
@@ -1,4 +1,4 @@
-from PartnerCreateUpdate import (
+from .PartnerCreateUpdate import (
     PartnerCreateView,
     PartnerUpdateView,
     PartnerUpdateParent,
@@ -28,7 +28,7 @@ from .DAEPartners import (
     DAEDetailView,
 )
 
-from PartnerDelete import PartnerDeleteView
-from PartnerDetail import PartnerDetailView
-from PartnerList import PartnerListView
-from PartnerAutoRegister import PartnerAutoRegister
+from .PartnerDelete import PartnerDeleteView
+from .PartnerDetail import PartnerDetailView
+from .PartnerList import PartnerListView
+from .PartnerAutoRegister import PartnerAutoRegister
