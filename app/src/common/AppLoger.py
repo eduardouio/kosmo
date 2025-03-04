2bcf93ece5cb2da0aaacf939ae6e11a53d3ba467
Eduardo Villota <eduardouio7@gmail.com>
2025-03-03 21:51:13 -0500

    Centralizamos todo en una funcion

--- app/src/common/AppLoger.py
+++ app/src/common/AppLoger.py
@@ -10,7 +10,7 @@ def check_file():
         os.makedirs(os.path.dirname(LOG_FILE_PATH), exist_ok=True)
         if not os.path.exists(LOG_FILE_PATH):
             with open(LOG_FILE_PATH, "w") as file:
-                pass  
+                pass
         return True
     except IOError as e:
         print(f"Error al crear el archivo: {e}")
@@ -27,3 +27,12 @@ def logging_error(message):
     check_file()
     with open(LOG_FILE_PATH, "a") as file:
         file.write(f"[{datetime.now()}] -> ERROR: {message}\n")
+
+
+def loggin(message, error=False):
+    if error:
+        logging_error(message)
+    else:
+        logging_message(message)
+
+    return True
