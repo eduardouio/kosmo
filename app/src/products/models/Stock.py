1f35732c664c19bae27a383f7eecafa57a7cbf88
Eduardo Villota <eduardouio7@gmail.com>
2025-04-14 17:47:03 -0500

    comentarios en stock

--- app/src/products/models/Stock.py
+++ app/src/products/models/Stock.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from common import BaseModel
 from partners.models import Partner
 from products.models import Product
+from common.AppLoger import loggin_event
 
 
 BOX_CHOICES = (
@@ -26,6 +27,7 @@ class StockDay(BaseModel):
     )
 
     def get_stock_day(self, date):
+        loggin_event(f'Buscando stock por fecha {date}')
         try:
             return self.objects.get(
                 date=date
@@ -35,8 +37,18 @@ class StockDay(BaseModel):
 
     @classmethod
     def disable(cls, stock_day):
+        loggin_event(f'Deshabilitando stock {stock_day}')
         stock_day.is_active = False
         stock_day.save()
+
+    @classmethod
+    def get_by_id(cls, id):
+        loggin_event(f"Buscando stock por id {id}")
+        try:
+            return cls.objects.get(pk=id)
+        except ObjectDoesNotExist:
+            loggin_event(f"El stock {id} no existe o fue eliminado")
+            raise Exception("Registro de stock Eliminado")
 
     def __str__(self):
         return str(self.date)
@@ -95,6 +107,7 @@ class StockDetail(BaseModel):
 
     @classmethod
     def get_partner_by_stock_day(cls, stock_day):
+        loggin_event(f'Buscando proveedores por stock {stock_day}')
         partners = cls.objects.filter(
             stock_day=stock_day,
             is_active=True
@@ -104,6 +117,9 @@ class StockDetail(BaseModel):
 
     @classmethod
     def get_total_boxes_by_model(cls, stock_day, box_model):
+        loggin_event(
+            f'Buscando cajas por stock {stock_day} y modelo {box_model}'
+        )
         stock_detail = cls.objects.filter(
             stock_day=stock_day,
             is_active=True
@@ -117,6 +133,9 @@ class StockDetail(BaseModel):
 
     @classmethod
     def get_stock_day_partner(cls, stock_day, partner):
+        loggin_event(
+            f'Buscando stock {stock_day} para el proveedor {partner}'
+        )
         return cls.objects.filter(
             stock_day=stock_day,
             partner=partner,
@@ -125,6 +144,9 @@ class StockDetail(BaseModel):
 
     @classmethod
     def disable_stock_detail(cls, stock_day, partner):
+        loggin_event(
+            f'Deshabilitando stock {stock_day} para el proveedor {partner}'
+        )
         stock_detail = cls.get_stock_day_partner(stock_day, partner)
         for i in stock_detail:
             i.is_active = False
@@ -141,6 +163,7 @@ class StockDetail(BaseModel):
 
     @classmethod
     def rebuild_stock_detail(cls, stock_detail):
+        loggin_event(f'Reconstruyendo stock {stock_detail}')
         box_items = BoxItems.get_box_items(stock_detail)
         total_stem_flower = 0
         total_cost_price = 0
@@ -154,6 +177,13 @@ class StockDetail(BaseModel):
         stock_detail.tot_cost_price_box = total_cost_price
         stock_detail.profit_margin = total_margin
         stock_detail.save()
+
+    @classmethod
+    def get_by_id(cls, id):
+        try:
+            return cls.objects.get(pk=id)
+        except ObjectDoesNotExist:
+            return None
 
 
 # -----------------------------------------------------------------------------
@@ -195,6 +225,7 @@ class BoxItems(BaseModel):
 
     @classmethod
     def get_box_items(cls, stock_detail):
+        loggin_event(f'Buscando cajas por stock {stock_detail}')
         return cls.objects.filter(
             stock_detail=stock_detail,
             is_active=True
