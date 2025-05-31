2fe6bec36f24bce483eadb1b23e2759d561a8289
Eduardo Villota <eduardouio7@gmail.com>
2025-05-31 17:11:39 -0500

    Corregir la lógica de eliminación de socios en PartnerDeleteView y mejorar la gestión de redirección tras la eliminación.

--- app/src/partners/views/PartnerDelete.py
+++ app/src/partners/views/PartnerDelete.py
@@ -3,18 +3,16 @@ from django.urls import reverse_lazy
 from partners.models import Partner
 from django.views.generic import RedirectView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from partners.models import Partner
 
 
 class PartnerDeleteView(LoginRequiredMixin, RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         partner = Partner.objects.get(pk=kwargs['pk'])
         try:
-            partner.delete()
-            url = reverse_lazy('partner_list')
+            partner.delete(partner.pk)  # Pasar el ID como parámetro requerido
+            url = str(reverse_lazy('partner_list'))  # Convertir a string
             return f'{url}?action=deleted'
         except Exception as e:
             print(e)
-            url = reverse_lazy('equipment_detail', kwargs={'pk': partner.pk})
-            url = ''.join([url, '?action=no_delete'])
-            return url
+            url = str(reverse_lazy('partner_detail', kwargs={'pk': partner.pk}))  # Corregir nombre de URL y convertir a string
+            return f'{url}?action=no_delete'
