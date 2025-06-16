d0aaab8bc233c42befd9b663e195ccc9e09898d8
Eduardo Villota <eduardouio7@gmail.com>
2025-06-16 14:55:36 -0500

    feat: agregar archivos favicon.ico e index.html al .gitignore y mejorar configuración de Vite

--- app/orders/vite.config.js
+++ app/orders/vite.config.js
@@ -1,18 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    // Directorio de salida principal para todos los assets generados por Vite
+    outDir: '../src/static/js/app', 
+    emptyOutDir: false, // Para no borrar otros archivos en outDir si los hubiera
+    rollupOptions: {
+      output: {
+        entryFileNames: 'app-order.js',
+        // Si se generan chunks (divisiones de código), también usarán este prefijo
+        chunkFileNames: 'app-order.[hash].js', 
+        assetFileNames: (assetInfo) => {
+          // Para los archivos CSS, nombrarlos específicamente
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'app-orders.css'; // Se generará como app-orders.css en outDir
+          }
+          // Para otros assets (imágenes, fuentes, etc.)
+          return 'assets/app-order.[name]-[hash].[ext]';
+        },
+        // Intentar generar un solo archivo JS si es posible (deshabilitando chunks manuales)
+        manualChunks: undefined 
+      }
+    }
+  }
 })
