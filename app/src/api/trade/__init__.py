ace67420cd52989f7a8d7d09ea3cf59d81a9f06e
Eduardo Villota <eduardouio7@gmail.com>
2025-07-31 22:02:44 -0500

    Agregar CollectionsCreateUpdateAPI y CollectionsDeleteAPI al módulo de API

--- app/src/api/trade/__init__.py
+++ app/src/api/trade/__init__.py
@@ -2,10 +2,14 @@ from .CollectionsContextAPI import CollectionsContextAPI
 from .PaymentContextData import PaymentContextData
 from .PaymentCreateUpdateAPI import PaymentCreateUpdateAPI
 from .PaymentDeleteAPI import PaymentDeleteAPI
+from .CollectionsCreateUpdateAPI import CollectionsCreateUpdateAPI
+from .CollectionsDeleteAPI import CollectionsDeleteAPI
 
 __all__ = [
     'CollectionsContextAPI',
     'PaymentContextData',
     'PaymentCreateUpdateAPI',
     'PaymentDeleteAPI',
+    'CollectionsCreateUpdateAPI',
+    'CollectionsDeleteAPI',
 ]
