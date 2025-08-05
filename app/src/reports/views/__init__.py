6effb7bb158cbe24e33d7f4385be68c1c80380e3
Eduardo Villota <eduardouio7@gmail.com>
2025-08-05 16:03:53 -0500

    Agregar vistas de informes de colecciones, pymen, ventas y compras al módulo de informes

--- app/src/reports/views/__init__.py
+++ app/src/reports/views/__init__.py
@@ -4,6 +4,10 @@ from .TemplateReportCusOrderView import TemplateReportCusOrderView
 from .PDFReportCusOrder import PDFReportCusOrder
 from .TemplateInvoice import TemplateInvoice
 from .PDFInvoice import PDFInvoice
+from .CollectionsReportsView import CollectionsReportsView
+from .PymenReportView import PymenReportView
+from .SalesReportView import SalesReportView
+from .PurchaseReportView import PurchaseReportView
 
 __all__ = [
     'PDFReportSupOrder',
@@ -12,4 +16,8 @@ __all__ = [
     'PDFReportCusOrder',
     'TemplateInvoice',
     'PDFInvoice',
+    'CollectionsReportsView',
+    'PymenReportView',
+    'SalesReportView',
+    'PurchaseReportView',
 ]
