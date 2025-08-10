a55b3b94011fddae16de4a4d32ae3cb41d25fd44
Eduardo Villota <eduardouio7@gmail.com>
2025-08-10 18:19:32 -0500

    envio de pes

--- app/src/accounts/urls.py
+++ app/src/accounts/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path, re_path
-from django.conf import settings
-from django.views.static import serve
-import os
-from .views import LoginTV, LogoutRV, HomeTV
+from .views import LoginTV, LogoutRV, HomeTV, UserProfileView
 from .views.CertValidationView import pki_validation_view
-from accounts import UserProfile
 
 urlpatterns = [
     path('', HomeTV.as_view(), name='home'),
     path('accounts/login/', LoginTV.as_view(), name='login'),
     path('accounts/logout/', LogoutRV.as_view(), name='logout'),
-    path('accounts/profile/', UserProfile.UserProfileView.as_view(), name='user_profile'),
-    re_path(r'^\.well-known/pki-validation/(?P<filename>.*)$', 
-            pki_validation_view,
-            name='pki_validation'),
+    path('accounts/profile/', UserProfileView.as_view(), name='user_profile'),
+    re_path(r'^\.well-known/pki-validation/(?P<filename>.*)$',pki_validation_view,name='pki_validation'),
 ]
