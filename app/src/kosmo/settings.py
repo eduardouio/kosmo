a0c45c5b51ee50a33736f70c77f98bc37cdefd92
Eduardo Villota <eduardouio7@gmail.com>
2025-08-13 06:59:47 -0500

    nuvoas accesos

--- app/src/kosmo/settings.py
+++ app/src/kosmo/settings.py
@@ -174,6 +174,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:5173',
+    'http://test.kosmoflowers.com:5050',
+    'https://test.kosmoflowers.com:5050'
 ]
 
 
