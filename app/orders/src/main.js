73835265b3fca3a12462ea228a03e86dbf8acc95
Eduardo Villota <eduardouio7@gmail.com>
2025-08-25 06:04:11 -0500

    feat: agregar plugin para observar cambios en stagesLoaded en Pinia

--- app/orders/src/main.js
+++ app/orders/src/main.js
@@ -6,9 +6,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Creamos Pinia y añadimos un plugin para observar cambios en stagesLoaded
+const pinia = createPinia();
+pinia.use(({ store }) => {
+	if (store.$id === 'baseStore') {
+		let prev = store.stagesLoaded;
+		store.$subscribe((mutation, state) => {
+			if (state.stagesLoaded !== prev) {
+				console.log(`[baseStore] stagesLoaded: ${prev} -> ${state.stagesLoaded}`);
+				prev = state.stagesLoaded;
+			}
+		}, { detached: true });
+	}
+});
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#appVue')
