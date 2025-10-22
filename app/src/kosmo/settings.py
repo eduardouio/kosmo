d07d6f37d5cfc6acb12fd9175b28099b4348473f
eduardouio <eduardouio7@gmail.com>
2025-10-22 05:26:21 -0500

    cambio: actualiza el código de idioma de 'es-Es' a 'en-US' en la configuración

--- app/src/kosmo/settings.py
+++ app/src/kosmo/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-Es'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'America/Guayaquil'
 
