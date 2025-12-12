ea9608a9c6901f67aa2f9d133e73006df98cb188
eduardo <eduardouio7@gmail.com>
2025-12-12 09:09:15 -0500

    cancelacion de factura

--- app/src/trade/views/DeleteInvoiceView.py
+++ app/src/trade/views/DeleteInvoiceView.py
@@ -1,38 +1,54 @@
-from django.urls import path, reverse
+from django.urls import reverse
 from django.views.generic import RedirectView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
-from trade.models import Invoice, Order
+from trade.models import Invoice
+from common.AppLoger import loggin_event
+from trade.models.Order import Order
 
 
-# trade/invoice/delete/<int:pk>/
 class DeleteInvoiceView(LoginRequiredMixin, RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
-        invoice_id = kwargs.get('pk')
+        loggin_event(
+            self.request.user,
+            "DELETE_INVOICE",
+            f"El usuario {self.request.user.username} ha eliminado la factura con ID {kwargs.get('pk')}",
+        )
+        
+        invoice_id = kwargs.get("pk")
+        invoice = Invoice.objects.get(pk=invoice_id)
 
-        try:
-            invoice = Invoice.objects.get(pk=invoice_id)
+        if not invoice.is_active:
+            raise Http404("La factura no existe o ya ha sido eliminada")
+        
+        invoice.order.status = "PENDIENTE"
+        Invoice.order.is_invoiced = False
+        Invoice.order.id_invoice = 0
+        Invoice.order.num_invoice = None
+        invoice.order.save()
 
-            # Verificar que la factura esté activa
-            if not invoice.is_active:
-                raise Http404("La factura no existe o ya ha sido eliminada")
+        parent_order = Order.objects.filter(parent_order = invoice.order.id).first()
+        parent_order.status = "PENDIENTE"
+        parent_order.is_invoiced = False
+        parent_order.id_invoice = 0
+        parent_order.num_invoice = None
+        parent_order.save()
 
-            # Marcar la factura como inactiva
-            invoice.is_active = False
-            invoice.status = 'ANULADO'
-            invoice.save()
+        purchase_invoice = Invoice.objects.filter(order = parent_order.id).first()
+        purchase_invoice.is_active = False
+        purchase_invoice.status = "ANULADO"
+        purchase_invoice.save()
 
-            # Actualizar la orden relacionada
-            order = invoice.order
-            order.status = 'PENDIENTE'
-            order.is_invoiced = False
-            order.id_invoice = 0
-            order.num_invoice = None
-            order.save()
+        Invoice.disable_invoice_items(invoice)
+        Invoice.disable_invoice_items(purchase_invoice)
+        
+        invoice.is_active = False
+        invoice.status = "ANULADO"
+        invoice.save()
 
-            # Redirigir a la vista de detalle de la orden
-            return reverse('order_detail_presentation', kwargs={'pk': order.id}) + '?action=deleted_related'
+        return (
+            reverse("order_detail_presentation", kwargs={"pk": invoice.order.id})
+            + "?action=deleted_related"
+        )
 
-        except Invoice.DoesNotExist:
-            raise Http404("La factura no existe")
